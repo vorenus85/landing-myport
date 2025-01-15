1675090239d76d13d2679664fa2dd6072549ae6c
Janos Perge <janos.perge@gmail.com>
2025-01-15 19:06:24 +0100

    add headerSection

---
+++ src/components/sections/HeaderSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="header-section">
+    <HeaderTopModule></HeaderTopModule>
+    <HeroModule></HeroModule>
+  </section>
+</template>
+
+<script setup>
+import HeaderTopModule from '../modules/HeaderTopModule.vue';
+import HeroModule from '../modules/HeroModule.vue';
+</script>
+
+<style lang="scss" scoped>
+.header-section {
+  background: var(--secondary-color);
+  padding: 55px 0;
+}
+</style>
