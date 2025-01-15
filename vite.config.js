35ffef45621640bd8ae9a16f98305a2ce684c787
Janos Perge <janos.perge@gmail.com>
2025-01-15 17:35:40 +0100

    use imageUrl composable

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
+dotenv.config();
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  // eslint-disable-next-line no-undef
+  base: process.env.VITE_BASE_URL,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
