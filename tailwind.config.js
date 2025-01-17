25064ef4a49d4079d5c98d410abe353537e1fe01
Janos Perge <janos.perge@gmail.com>
2025-01-17 22:28:27 +0100

    fix containers

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ export default {
       center: true,
       padding: {
         DEFAULT: '1rem'
+      },
+      /* deafults
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px'
+      }
+        */
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1280px'
       }
     }
   },
